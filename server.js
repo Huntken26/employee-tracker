c33d2060ab5807546d48cc4b24f5cdbb0b69540e
huntken26 <huntken26@gmail.com>
2022-11-22 18:23:03 -0600

    Added gitignore, dotenv file, connection for sql

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const consoleTable = require('console.table');
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
-// const db = mysql.createConnection(
-//   {
-//     host: 'localhost',
-//     // MySQL username,
-//     user: 'root',
-//     // MySQL password
-//     password: '',
-//     database: 'classlist_db'
-//   },
-//   console.log(`Connected to the classlist_db database.`)
-// );
+//Connect to database. Used code from one of ourclass activity files
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password and db from env file
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  console.log(`Connected to the twitter_db database.`)
+);
 
 // // Query database
 // db.query('SELECT * FROM students', function (err, results) {
